ea4e631719dedcd21605aec6dcf3659bbaea5db9
Mikhail Korolev <37420156+DuDlikSPB@users.noreply.github.com>
2020-02-24 23:25:51 +0100

    Add files via upload

--- js/ryaba.js
+++ js/ryaba.js
@@ -1,48 +1,31 @@
 const dataURL = "https://api.myjson.com/bins/jcmhn";
-const fields = [
-	"var1",
-	"var2",
-	"var3",
-	"var4",
-	"var5",
-	"var6",
-	"speach"
-]
 
-function getFormValues() {
-	let obj = {};
-
-	fields.forEach(function(field) {
-		obj[field] = $("input[name=" + field + "]")[0].value
-	});
-
-	return obj;
+function handleButton() {
+  $.getJSON(dataURL, function (json_data) {
+    data = json_data['text'];
+    let final_text = '';
+    let var1 = $('input[name=var1]')[0]['value'];
+    let var2 = $('input[name=var2]')[0]['value'];
+    let var3 = $('input[name=var3]')[0]['value'];
+    let var4 = $('input[name=var4]')[0]['value'];
+    let var5 = $('input[name=var5]')[0]['value'];
+    let var6 = $('input[name=var6]')[0]['value'];
+    let speach = $('input[name=speach]')[0]['value'];
+    
+    data.forEach(function(item, index){
+      item = item.replace('{var1}', var1);
+      item = item.replace('{var2}', var2);
+      item = item.replace('{var3}', var3);
+      item = item.replace('{var4}', var4);
+      item = item.replace('{var5}', var5);
+      item = item.replace('{var6}', var6);
+      item = item.replace('{speach}', speach);
+      final_text = final_text + item + '<br>';
+    });
+    $('div#result').html(final_text);
+    }
+);
 }
-
-function handleButton(event) {
-	$.getJSON(dataURL, handleData);
-	$("form").hide();
-
-	event.preventDefault();
-}
-
-function handleData(data) {
-	let message = "";
-
-	let values = getFormValues();
-
-	data["text"].forEach(function(line) {
-		for (key in values) {
-			line = line.replace("{" + key + "}", values[key]);
-			// line = line.replace(`{${key}}`, values[key]);
-		}
-
-		message = message + line + "<BR>";
-	});
-
-    $("div#result").html(message);
-}
-
 function init() {
 	$("#button-fetch").click(handleButton);
 }
